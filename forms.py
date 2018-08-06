a7b0f2de742fd3a53ef35bc04472e922e8db5af6
charliekaks <kaksunchained@gmail.com>
2018-08-06 12:30:40 +0300

    Added the forms and more routes to the application and also added postgres mpdel

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(FlaskForm):
+    first_name = StringField('First Name',validators=[DataRequired('Please enter first name')])
+    last_name = StringField('Last Name',validators=[DataRequired('Please enter last name')])
+    email = StringField('Email',validators=[DataRequired('Please enter your email'),Email()])
+    password = PasswordField('Password',validators=[DataRequired('Please enter password'),Length(min=6,message="Must be at least 6 characters")])
+    submit = SubmitField('Sign Up')
--- routes.py
+++ routes.py
@@ -1,13 +1,35 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
-Bootstrap(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///learningflask"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+app.secret_key = 'development-key'
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if request.method == 'GET':
+        return render_template("signup.html", form = form)
+    elif request.method  == 'POST':
+        if form.validate() == False:
+            return render_template("signup.html", form= form)
+        else:
+            new_user = User(form.first_name.data,form.last_name.data,form.email.data,form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            return 'Success'
 
 if __name__ == "__main__":
     app.run(debug=True)
